b9b7e9f494f5ac1374aaf25835248ff3f988ea35
Krj <mailbox_krj@163.com>
2020-02-06 17:18:32 +0800

    data段、code段等所有section重定位，并修正关于print_screen函数的逻辑漏洞

---
+++ 1stQuarter_BarBoard/11_CodeRelocation_to_SDRAM_all_section/include/soc_s3c2440/soc_s3c2440_public.h
@@ -0,0 +1,10 @@
+#ifndef _SOC_S3C2440_PUBLIC_H_
+#define _SOC_S3C2440_PUBLIC_H_
+
+#include "soc_s3c2440_reg.h"
+#include "soc_s3c2440_reg_operator.h"
+#include "soc_s3c2440_gpio_field.h"
+#include "soc_s3c2440_uart_field.h"
+#include "soc_s3c2440_other_field.h"
+
+#endif
